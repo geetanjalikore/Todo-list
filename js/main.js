e5a5a031b06e2ac38ff744b6e89ad8a91186b374
Geetanjali Kore <geetakore04@gmail.com>
2020-08-12 11:21:47 +0530

    unordered list for todo completed list and uncompleted list in div tag class name as container. completed styling for list items.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-console.log("Hello world");
-console.log(document.getElementById('add'));
+//User clicked on button 
+// if the item ha value, then add it to the todo list 
+document.getElementById('add').addEventListener('click',function(){
+    var value1=document.getElementById('item').value;
+    if(value1){
+        console.log(value1);
+    }
+});
